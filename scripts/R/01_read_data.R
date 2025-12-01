354e05b2f3e73600ded4ad2148a13eef55af9179
Anurag <angshr667@gmail.com>
2025-12-01 12:35:15 +0530

    Added structure of analysis and data

---
+++ scripts/R/01_read_data.R
@@ -0,0 +1,14 @@
+# Import required libraries
+
+
+
+
+# Import raw data into objects using assignment operator
+
+
+
+# Data transformation using tidyverse- dplyr package
+
+  # ascending arrange verb can be used from dplyr to arrange based on the occurence i.e. >= or <= 
+  # remove duplicate rows if any with distinct() verb
+
---
+++ scripts/R/02_id_harmonization.R
@@ -0,0 +1,6 @@
+# Map each database’s IDs into a unified set of (Ensemble IDs)
+
+
+
+
+# Build the main tidy table: rbp_records_long.tsv
