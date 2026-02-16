3c25dccfd0e2f6110239b9098c2eb07ac26df7f4
Anurag <angshr667@gmail.com>
2026-02-16 11:38:23 +0530

    Updated gitignore and project files

---
+++ scripts/R/04_ploting_venn.R
@@ -0,0 +1,12 @@
+library(ggVennDiagram)
+library(ggplot2)
+library(dplyr)
+library(sf)
+
+# Define sets
+sets <- list(
+  RBP_GO  = rbpgo    %>% distinct(gene_name) %>% pull(gene_name),
+  RBP_IMG = rbpimage %>% distinct(gene_name) %>% pull(gene_name),
+  RBP_WRLD = rbpworld %>% distinct(gene_name) %>% pull(gene_name)
+)
+
